c645cac5872aacf0701961a8db185d24cb141ad6
JMIKEPAD <mchparodi@gmail.com>
2022-02-11 13:27:01 +0100

    exercise classroom

--- console.js
+++ console.js
@@ -1,51 +1,58 @@
 "use strict" ///// somma dell'array di numeri
 const fs = require('fs');
-const { json } = require('stream/consumers');
-const {Parse}= require('./parser.js');
+const {ParseV2}= require('./parserV2.js');
 
-
+// fs.writeFile(fileToWrite, json, error =>{
+//       if (error){
+//         console.log(error);
+//       }else{
+//         console.log('bella storia !!!!!!!!!');
+//       }
+//     })
+  
 
 const arg = process.argv.slice(2);
 
 const fileToRead = arg[0];
-const fileToWrite = arg[1]
+const fileToWrite = arg[1];
+// const outputType = arg [2];
+// fs.readFile(fileToRead, 'utf8',(error,data) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(data);
+//     let array5 = ParseV2.parseToObject(data);
+//     const json = JSON.stringify(array5);
+//     writeFileAsync(json);
+//   }
+// })
+// function  writeFileAsync(json){
+//   fs.writeFile(fileToWrite, json, error =>{
+//     if (error){
+//       console.log(error);
+//     }else{
+//       console.log('bella storia !!!!!!!!!');
+//     }
+//   })
+// }
 
-fs.readFile(fileToRead, 'utf8',(error,data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-    let array5 = Parse.parserAll(data);
-    const json = JSON.stringify(array5);
-    writeFileAsync(json);
-  }
-})
-function  writeFileAsync(json){
-  fs.writeFile(fileToWrite, json, error =>{
-    if (error){
-      console.log(error);
-    }else{
-      console.log('bella storia !!!!!!!!!');
-    }
-  })
-}
+let array1 = [];
 
-
-// let array1 = Parse.parseCSVLine(data);
-// try {
-//   const data = fs.readFileSync(fileToRead, 'utf8')
-//     console.log(data)
-//   } catch (err) {
-//     console.error(err)
-//   } 
+try {
+  const data = fs.readFileSync(fileToRead, 'utf8')
+  array1 = ParseV2.parseToObject(data);
+    console.log(array1)
+  } catch (err) {
+    console.error(err)
+  } 
 
 // console.log(array1);
 
-// try {
-//     fs.writeFileSync(fileToWrite, JSON.stringify(array1))
-// } catch (error) {
-//     console.log(error);
-// }
+try {
+    fs.writeFileSync(fileToWrite, JSON.stringify(array1))
+} catch (error) {
+    console.log(error);
+}
 
 
 
--- parser.js
+++ parser.js
@@ -11,7 +11,7 @@ class InvalidStringError extends Error {
 }
 
 class PartialStringError extends Error {
-    constructor(message, partialResult){
+    constructor(message, partialResult) {
         super(message);
         this.partialResult = partialResult;
     }
@@ -20,57 +20,83 @@ class PartialStringError extends Error {
 
 
 
-class Parse{
-    
+class Parse {
+
     /**
      * * Given a string, it returns an array of floats
      * @param string - The string to be parsed.
      * @returns The array of float numbers
      */
 
-static parserAll(csv){
+    static parserAll(csv) {
 
-    const stringwithoutblanckspace = Parse.removeBlanckSpace(csv)
+        const stringwithoutblanckspace = Parse.removeBlanckSpace(csv)
 
-    const cleanString= Parse.replaceCommaAtDot(stringwithoutblanckspace);
+        const cleanString = Parse.replaceCommaAtDot(stringwithoutblanckspace);
 
-    const stringNewLine =Parse.splitForNewLine(cleanString);
+        const stringNewLine = Parse.splitForNewLine(cleanString);
 
-    let array2 = [];
+        let array2 = [];
 
-    for (const line of stringNewLine) {
-       const lineArray = Parse.parseCSVLine(line)
-        array2 = array2.concat(lineArray);
+        for (const line of stringNewLine) {
+            const lineArray = Parse.parseCSVLine(line)
+            array2 = array2.concat(lineArray);
+
+        }
+        return array2;
 
     }
-    return array2;
+    static parserToMatrix(csv) {
 
-}
+        const stringwithoutblanckspace = Parse.removeBlanckSpace(csv)
 
-    static parseCSVLine(line){
-        
+        const cleanString = Parse.replaceCommaAtDot(stringwithoutblanckspace);
+
+        const stringNewLine = Parse.splitForNewLine(cleanString);
+
+        let array2 = [];
+
+        for (const line of stringNewLine) {
+            const lineArray = Parse.parseCSVLine(line)
+            array2.push(lineArray);
+
+        }
+        return array2;
+
+    }
+
+
+    static parseCSV(csv, outputType)
+
+    static parseCSVLine(line) {
+
         const arrayOfStrings = Parse.splitForSemicolon(line);
-       
+
         const array6 = [];
 
-      for (const word of arrayOfStrings) {
-          const value = Parse.parseWord(word)
+        for (const word of arrayOfStrings) {
+            const value = Parse.parseWord(word)
+            if (outputType === "-a") {
+                array6 = array6.concat(lineArray);
+            } else {
+                array6.push(value);
 
-        array6.push(value);
+            }
 
-      }
+        }
 
-        
+
+
         return array6; //.reduce((p,c)=> p + c);
 
     }
 
-    static parseWord(word){
+    static parseWord(word) {
         if (!isNaN(word)) {
             return parseFloat(word);
         }
 
-        if (word.toLoweCase() === 'true' || word.toLoweCase() === 'false' ) {
+        if (word.toLoweCase() === 'true' || word.toLoweCase() === 'false') {
             return word.toLoweCase() === 'true';
         }
 
@@ -83,12 +109,12 @@ static parserAll(csv){
 
 
 
-   /**
-    * Split a string into an array of strings using a semicolon as the delimiter
-    * @param string - The string to be split.
-    * @returns an array of strings.
-    */
-   
+    /**
+     * Split a string into an array of strings using a semicolon as the delimiter
+     * @param string - The string to be split.
+     * @returns an array of strings.
+     */
+
 
 
 
@@ -105,19 +131,19 @@ static parserAll(csv){
         return replaced;
     }
 
-    static parseArrayToFloat(array){
-        let tempArray=[];
+    static parseArrayToFloat(array) {
+        let tempArray = [];
         for (let i = 0; i < array.length; i++) {
             const string = array[i];
-            const parsed= parseFloat(string);
+            const parsed = parseFloat(string);
             if (!isNaN(parsed)) {
                 tempArray.push(parsed);
             }
-            
+
         }
         return tempArray;
     }
-    static splitForNewLine(string){
+    static splitForNewLine(string) {
         const splittedStringOnNewLine = string.split(/\n?\r/);
         return splittedStringOnNewLine;
     }
@@ -126,8 +152,8 @@ static parserAll(csv){
         const splittedString = array.split(";");
         return splittedString;
     }
-   
+
 }
 
 
-module.exports = {Parse};
+module.exports = { Parse };
